77018268b97e6aa72c8a9e02cf730d8bba2217ce
git-clay <ce.smith5525@gmail.com>
2017-01-30 13:17:14 -0700

    no idea if anything changed but the picture button works on an emulator

--- photo_ionic/www/js/controllers.js
+++ photo_ionic/www/js/controllers.js
@@ -23,18 +23,35 @@ function LandmarkCtrl($scope,$http){
 }
 
 CameraCtrl.$inject = ['$scope','$cordovaCamera']
+
 function CameraCtrl($scope,$cordovaCamera){
-	$scope.pictureUrl = "http://placehold.it/300x300"
+console.log($cordovaCamera)
+  document.addEventListener("deviceready", function () {
 
-	$scope.takePicture =function(){
-		console.log($cordovaCamera)
-		$cordovaCamera.getPicture({})
-		.then(function(data){
-			console.log('camera data: '+angular.toJson(data))
-		}), function(err){
-			console.log('camera error: '+angular.toJson(err))
-
-		}
-	}
+ $scope.takePicture = function() {
+        var options = { 
+            quality : 75, 
+            destinationType : Camera.DestinationType.DATA_URL, 
+            sourceType : Camera.PictureSourceType.CAMERA, 
+            allowEdit : true,
+            encodingType: Camera.EncodingType.JPEG,
+            targetWidth: 300,
+            targetHeight: 300,
+            popoverOptions: CameraPopoverOptions,
+            saveToPhotoAlbum: false
+        };
+ 
+        $cordovaCamera.getPicture(options).then(function(imageData) {
+            $scope.imgURI = "data:image/jpeg;base64," + imageData;
+            console.log($scope.imgURI)
+        }, function(err) {
+			console.log(err)
+	    });
+    }
+    })
 
 }
+
+
+
+
