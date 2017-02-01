2975aa8d7b5a2d4ce7650cb102279b6571be8ab3
git-clay <ce.smith5525@gmail.com>
2017-01-31 21:30:57 -0700

    same

--- controllers/landmark.js
+++ controllers/landmark.js
@@ -5,7 +5,7 @@ face_id ='a1e463df6f54932abc7a424d6f7a5bd1',//this comes from api/detect
 file_image_url = 'https%3A%2F%2Fs-media-cache-ak0.pinimg.com%2Foriginals%2F6d%2F84%2F9a%2F6d849a537527cd121e5330c40bec2f6e.png';
 
 function getApi(req,res){
-	console.log(req)
+	// console.log(req)
 unirest.get(base_url+"glass%2Cpose%2Cgender%2Cage%2Crace%2Csmiling&url="+file_image_url)
 .header("X-Mashape-Key", mashapeKey)
 .header("Accept", "application/json")
@@ -13,7 +13,7 @@ unirest.get(base_url+"glass%2Cpose%2Cgender%2Cage%2Crace%2Csmiling&url="+file_im
   console.log(result.status, result.headers, result.body);
   res.header("Access-Control-Allow-Origin", "*");
   console.log(result.body)
-  face_id=result.body;
+  // var faceId=result.body;
   getLandmark(face_id)
 });
 
