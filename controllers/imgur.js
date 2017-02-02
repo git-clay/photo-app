be6787fcfedf9d3c7aa46f68b46348b2faa5fbc3
git-clay <ce.smith5525@gmail.com>
2017-02-02 10:01:30 -0700

    trying to send string to api

--- controllers/imgur.js
+++ controllers/imgur.js
@@ -9,7 +9,7 @@ function getApi(req,res){
 	var imageData = req.body
 	var newImageData =JSON.stringify(imageData)
 	console.log('.info:',Object.keys(imageData))
-	console.log('.stuff:',imageData[0].stuff)
+	console.log('.stuff:',imageData.stuff)
 	console.log('newimage:',newImageData, typeof newImageData)
 
 var options ={
